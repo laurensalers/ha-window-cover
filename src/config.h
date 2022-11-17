7a89ad8588b3db1abeabac1bc28b70445b4714d1
Laurens Alers <l.alers@calderholding.nl>
2022-11-17 21:01:29 +0100

    fixes

--- src/config.h
+++ src/config.h
@@ -1,1 +1,5 @@
 #define WM_ACCESSPOINT_PASSWORD "cynu4c9r"
+
+#define STEP_PIN 1
+#define DIR_PIN 2
+#define ENABLE_PIN 3
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
+#include <AccelStepper.h>
 #include <MQTT.h>
 
 #include <Config.h>
@@ -8,19 +9,27 @@
 #include <ObservableValue.h>
 
 // Topics
+const char *topicStepperPositionSavedGet = "stepperPositionSaved/get";
 const char *topicStepperPositionGet = "stepperPosition/get";
+const char *topicStepperPositionSet = "stepperPosition/set";
 const char *topicStepperPositionMaxGet = "stepperPositionMax/get";
+const char *topicStepperPositionMaxSet = "stepperPositionMax/set";
+const char *topicPositionGet = "position/get";
+const char *topicPositionSet = "position/set";
 
 char deviceName[50];
+bool restoreSavedStepperPosition = false;
 
 WiFiClient net;
 WiFiManager wifiManager;
 MQTTClient client;
 MqttQueueHandler mqttQueueHandler(&client, deviceName);
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 // Observables
-ObservableValue stepperPosition(0);
-ObservableValue stepperPositionMax(0);
+ObservableValue<long> stepperPosition(0);
+ObservableValue<long> stepperPositionMax(0);
+ObservableValue<byte> position(0); // In percents
 
 bool connectMqtt()
 {
@@ -30,6 +39,69 @@ bool connectMqtt()
   }
 
   return client.connect(deviceName, "esp32", "cynu4c9r");
+}
+
+void handleMqttMessage(String &topic, String &payload)
+{
+  if (restoreSavedStepperPosition && topic.endsWith(topicStepperPositionSavedGet))
+  {
+    stepperPosition.setValue(payload.toInt());
+    stepper.setCurrentPosition(stepperPosition.value());
+    restoreSavedStepperPosition = false;
+    return;
+  }
+
+  if (topic.endsWith(topicStepperPositionMaxSet))
+  {
+    stepperPositionMax.setValue(payload.toInt());
+    return;
+  }
+
+  if (topic.endsWith(topicPositionSet))
+  {
+    int targetPosition = payload.toInt();
+    int sanitizedTargetPosition = max(min(targetPosition, 100), 0);
+
+    float stepperPosition = (sanitizedTargetPosition / 100) * stepperPositionMax.value();
+
+    stepper.moveTo(stepperPosition);
+    return;
+  }
+}
+
+void configureStepper()
+{
+  // TODO:
+  // stepper.setEnablePin(ENABLE_PIN);
+  // stepper.setAcceleration();
+  // stepper.setMaxSpeed();
+
+  stepper.disableOutputs();
+}
+
+void calculatePosition()
+{
+  if (stepperPositionMax.value() == 0)
+  {
+    position.setValue(0);
+    return;
+  }
+
+  float pos = (stepperPositionMax.value() / stepperPosition.value()) * 100;
+  position.setValue(pos);
+}
+
+void bindObservers()
+{
+  stepperPosition.addObserver([](long value)
+                              { mqttQueueHandler.queueMessage(topicStepperPositionGet, value); 
+                                mqttQueueHandler.queueMessage(topicStepperPositionSavedGet, value, true); });
+
+  stepperPositionMax.addObserver([](long value)
+                                 { mqttQueueHandler.queueMessage(topicStepperPositionMaxGet, value); });
+
+  position.addObserver([](byte value)
+                       { mqttQueueHandler.queueMessage(topicPositionGet, value); });
 }
 
 void setup()
@@ -43,20 +115,24 @@ void setup()
   wifiManager.autoConnect(deviceName, WM_ACCESSPOINT_PASSWORD);
   wifiManager.startWebPortal();
 
-  // Bind mqtt to observables
-  stepperPosition.addObserver([](int value)
-                              { mqttQueueHandler.queueMessage(topicStepperPositionGet, value); });
-  stepperPositionMax.addObserver([](int value)
-                                 { mqttQueueHandler.queueMessage(topicStepperPositionMaxGet, value); });
+  // Bind mqtt to observabless
+  bindObservers();
 
   // MQTT Config
   client.begin("homeassistant.lan", 1883, net);
+  client.onMessage(handleMqttMessage);
+
+  client.subscribe(topicStepperPositionSet);
+  client.subscribe(topicStepperPositionMaxSet);
+
+  configureStepper();
 }
 
 void triggerObservers()
 {
   stepperPosition.trigger();
   stepperPositionMax.trigger();
+  position.trigger();
 }
 
 void loop()
@@ -67,7 +143,12 @@ void loop()
     triggerObservers();
   }
 
+  // Other
   wifiManager.process();
   client.loop();
   mqttQueueHandler.loop();
+
+  // Stepper
+  stepper.run();
+  stepperPosition.setValue(stepper.currentPosition());
 }
